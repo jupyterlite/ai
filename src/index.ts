15b6de565429273e0b159fa1a66712575449605d
Nicolas Brichet <32258950+brichet@users.noreply.github.com>
2025-03-31 12:05:31 +0200

    Stop streaming (#61)
    * Add a button to stop streaming
    
    * Update dependency to jupyter-chat 0.9.0

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,8 @@ import {
   buildErrorWidget,
   ChatCommandRegistry,
   IActiveCellManager,
-  IChatCommandRegistry
+  IChatCommandRegistry,
+  InputToolbarRegistry
 } from '@jupyter/chat';
 import {
   JupyterFrontEnd,
@@ -28,6 +29,7 @@ import { defaultProviderPlugins } from './default-providers';
 import { AIProviderRegistry } from './provider';
 import { aiSettingsRenderer, SettingConnector } from './settings';
 import { IAIProviderRegistry } from './tokens';
+import { stopItem } from './components/stop-button';
 
 const chatCommandRegistryPlugin: JupyterFrontEndPlugin<IChatCommandRegistry> = {
   id: '@jupyterlite/ai:autocompletion-registry',
@@ -102,12 +104,30 @@ const chatPlugin: JupyterFrontEndPlugin<void> = {
       });
 
     let chatWidget: ReactWidget | null = null;
+
+    const inputToolbarRegistry = InputToolbarRegistry.defaultToolbarRegistry();
+    const stopButton = stopItem(() => chatHandler.stopStreaming());
+    inputToolbarRegistry.addItem('stop', stopButton);
+
+    chatHandler.writersChanged.connect((_, users) => {
+      if (
+        users.filter(user => user.username === chatHandler.personaName).length
+      ) {
+        inputToolbarRegistry.hide('send');
+        inputToolbarRegistry.show('stop');
+      } else {
+        inputToolbarRegistry.hide('stop');
+        inputToolbarRegistry.show('send');
+      }
+    });
+
     try {
       chatWidget = buildChatSidebar({
         model: chatHandler,
         themeManager,
         rmRegistry,
-        chatCommandRegistry
+        chatCommandRegistry,
+        inputToolbarRegistry
       });
       chatWidget.title.caption = 'Jupyterlite AI Chat';
     } catch (e) {
