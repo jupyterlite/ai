1e4e5db416ab74358b3fcc5b35d1e8e8eb2e02a1
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2025-05-24 11:30:22 -0400

    Improve Mistral completions

--- src/default-providers/MistralAI/completer.ts
+++ src/default-providers/MistralAI/completer.ts
@@ -2,52 +2,21 @@ import {
   CompletionHandler,
   IInlineCompletionContext
 } from '@jupyterlab/completer';
-import { BaseChatModel } from '@langchain/core/language_models/chat_models';
-import {
-  BaseMessage,
-  HumanMessage,
-  SystemMessage
-} from '@langchain/core/messages';
-import { ChatMistralAI } from '@langchain/mistralai';
-import { Throttler } from '@lumino/polling';
+import { BaseLanguageModel } from '@langchain/core/language_models/base';
+import { MistralAI } from '@langchain/mistralai';
 
 import { BaseCompleter, IBaseCompleter } from '../../base-completer';
 import { COMPLETION_SYSTEM_PROMPT } from '../../provider';
 
 /**
- * The Mistral API has a rate limit of 1 request per second
+ * The completer for the MistralAI model.
  */
-const INTERVAL = 1000;
-
 export class CodestralCompleter implements IBaseCompleter {
   constructor(options: BaseCompleter.IOptions) {
-    this._completer = new ChatMistralAI({ ...options.settings });
-    this._throttler = new Throttler(
-      async (messages: BaseMessage[]) => {
-        const response = await this._completer.invoke(messages);
-        // Extract results of completion request.
-        const items = [];
-        if (typeof response.content === 'string') {
-          items.push({
-            insertText: response.content
-          });
-        } else {
-          response.content.forEach(content => {
-            if (content.type !== 'text') {
-              return;
-            }
-            items.push({
-              insertText: content.text
-            });
-          });
-        }
-        return { items };
-      },
-      { limit: INTERVAL }
-    );
+    this._completer = new MistralAI({ ...options.settings });
   }
 
-  get completer(): BaseChatModel {
+  get completer(): BaseLanguageModel {
     return this._completer;
   }
 
@@ -65,23 +34,33 @@ export class CodestralCompleter implements IBaseCompleter {
     request: CompletionHandler.IRequest,
     context: IInlineCompletionContext
   ) {
-    const { text, offset: cursorOffset } = request;
-    const prompt = text.slice(0, cursorOffset);
-
-    const messages: BaseMessage[] = [
-      new SystemMessage(this._prompt),
-      new HumanMessage(prompt)
-    ];
-
     try {
-      return await this._throttler.invoke(messages);
+      const { text, offset: cursorOffset } = request;
+      const prompt = text.slice(0, cursorOffset);
+      const suffix = text.slice(cursorOffset);
+
+      const response = await this._completer.completionWithRetry(
+        {
+          prompt,
+          model: this._completer.model
+        },
+        {
+          suffix
+        },
+        false
+      );
+      const items = response.choices.map(choice => {
+        return {
+          insertText: choice.message.content
+        };
+      });
+      return { items };
     } catch (error) {
       console.error('Error fetching completions', error);
       return { items: [] };
     }
   }
 
-  private _throttler: Throttler;
-  private _completer: ChatMistralAI;
+  private _completer: MistralAI;
   private _prompt: string = COMPLETION_SYSTEM_PROMPT;
 }
