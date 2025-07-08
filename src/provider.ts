73fec13c5ab2fe3ef2d7ff3479b5ed2d8d0b70b6
Nicolas Brichet <nicolas.brichet@quantstack.net>
2025-07-08 15:54:35 +0200

    Fix and clean the provider registry

--- src/provider.ts
+++ src/provider.ts
@@ -24,36 +24,6 @@ import { AIChatModel, AICompleter } from './types/ai-model';
 
 const SECRETS_NAMESPACE = PLUGIN_IDS.providerRegistry;
 const NOTIFICATION_DELAY = 2000;
-export const chatSystemPrompt = (
-  options: AIProviderRegistry.IPromptOptions
-) => `
-You are Jupyternaut, a conversational assistant living in JupyterLab to help users.
-You are not a language model, but rather an application built on a foundation model from ${options.provider_name}.
-You are talkative and you provide lots of specific details from the foundation model's context.
-You may use Markdown to format your response.
-If your response includes code, they must be enclosed in Markdown fenced code blocks (with triple backticks before and after).
-If your response includes mathematical notation, they must be expressed in LaTeX markup and enclosed in LaTeX delimiters.
-All dollar quantities (of USD) must be formatted in LaTeX, with the \`$\` symbol escaped by a single backslash \`\\\`.
-- Example prompt: \`If I have \\\\$100 and spend \\\\$20, how much money do I have left?\`
-- **Correct** response: \`You have \\(\\$80\\) remaining.\`
-- **Incorrect** response: \`You have $80 remaining.\`
-If you do not know the answer to a question, answer truthfully by responding that you do not know.
-The following is a friendly conversation between you and a human.
-`;
-
-export const COMPLETION_SYSTEM_PROMPT = `
-You are an application built to provide helpful code completion suggestions.
-You should only produce code. Keep comments to minimum, use the
-programming language comment syntax. Produce clean code.
-The code is written in JupyterLab, a data analysis and code development
-environment which can execute code extended with additional syntax for
-interactive features, such as magics.
-Only give raw strings back, do not format the response using backticks.
-The output should be a single string, and should only contain the code that will complete the
-give code passed as input, no explanation whatsoever.
-Do not include the prompt in the output, only the string that should be appended to the current input.
-Here is the code to complete:
-`;
 
 export class AIProviderRegistry implements IAIProviderRegistry {
   /**
@@ -318,8 +288,8 @@ export class AIProviderRegistry implements IAIProviderRegistry {
   async setChatProvider(settings: ReadonlyPartialJSONObject): Promise<void> {
     this.chatError = '';
     if (!Object.keys(settings).includes('provider')) {
-      Private.setName('completer', 'None');
-      Private.setCompleter(null);
+      Private.setName('chat', 'None');
+      Private.setChatModel(null);
       this.chatError = 'The provider is missing from the chat settings';
       return;
     }
