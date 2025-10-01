70bc9827cdd0900c97bc377b9566d4f817f7de98
Nicolas Brichet <nicolas.brichet@quantstack.net>
2025-10-01 16:59:30 +0200

    Allow moving chat to main area

---
+++ src/widgets/mainAreaChat.ts
@@ -0,0 +1,35 @@
+import { ChatWidget } from '@jupyter/chat';
+import { CommandToolbarButton, MainAreaWidget } from '@jupyterlab/apputils';
+import { launchIcon } from '@jupyterlab/ui-components';
+import { CommandRegistry } from '@lumino/commands';
+
+import { AIChatModel } from '../chat-model';
+import { CommandIds } from '../tokens';
+
+export namespace MainAreaChat {
+  export interface IOptions extends MainAreaWidget.IOptions<ChatWidget> {
+    commands: CommandRegistry;
+  }
+}
+
+export class MainAreaChat extends MainAreaWidget<ChatWidget> {
+  constructor(options: MainAreaChat.IOptions) {
+    super(options);
+    this.title.label = this.content.model.name;
+    this.toolbar.addItem(
+      'moveToSide',
+      new CommandToolbarButton({
+        commands: options.commands,
+        id: CommandIds.openChat,
+        args: {
+          name: this.content.model.name
+        },
+        icon: launchIcon
+      })
+    );
+  }
+
+  get model(): AIChatModel {
+    return this.content.model as AIChatModel;
+  }
+}
