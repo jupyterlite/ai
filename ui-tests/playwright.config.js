f9186f0a1aacba2195be4b5e1aeb3d15054201a4
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2025-10-08 19:36:02 +0200

    Add PWVIDEO and PWSLOWMO to record videos from UI tests (#181)

--- ui-tests/playwright.config.js
+++ ui-tests/playwright.config.js
@@ -5,6 +5,13 @@ const baseConfig = require('@jupyterlab/galata/lib/playwright-config');
 
 module.exports = {
   ...baseConfig,
+  use: {
+    ...baseConfig.use,
+    video: process.env.PWVIDEO || 'retain-on-failure',
+    launchOptions: {
+      slowMo: process.env.PWSLOWMO ? parseInt(process.env.PWSLOWMO) : 0
+    }
+  },
   webServer: {
     command: 'jlpm start',
     url: 'http://localhost:8888/lab',
