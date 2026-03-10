c40867381b6905eecc3d36ba4f06badd7bb4ae3d
Nicolas Brichet <nicolas.brichet@quantstack.net>
2026-03-10 18:15:35 +0100

    lint

--- src/index.ts
+++ src/index.ts
@@ -334,7 +334,6 @@ const chatModelHandler: JupyterFrontEndPlugin<IChatModelHandler> = {
     providerRegistry?: IProviderRegistry,
     toolRegistry?: IToolRegistry
   ): IChatModelHandler => {
-
     return new ChatModelHandler({
       settingsModel,
       agentManagerFactory,
